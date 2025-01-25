97840fca29beaf2fedec29636c47212e741a3f58
Ysyzdyko <Hamidkali@mail.ru>
2025-01-25 19:33:19 +0500

    fix : repo/menu/ interface

--- internal/storage/module.go
+++ internal/storage/module.go
@@ -8,8 +8,8 @@ type DataRepository interface {
 
 type MenuRepository interface {
 	GetAllMenuItems() ([]*entity.MenuItem, error)
-	GetMenuItemByID(id int) (entity.MenuItem, error)
-	CreateMenuItem(item entity.MenuItem) error
-	UpdateMenuItem(item entity.MenuItem) error
+	GetMenuItemByID(id int) (*entity.MenuItem, error)
+	CreateMenuItem(item *entity.MenuItem) error
+	UpdateMenuItem(item *entity.MenuItem) error
 	DeleteMenuItem(id int) error
 }
