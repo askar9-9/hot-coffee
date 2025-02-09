534dd4795015d3a539c31d71a51a3efc5a620a2a
askar9-9 <askaromarbekov9.9@gmail.com>
2025-02-09 17:55:05 +0500

    feat: basic logic

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"hot-coffee/internal/server"
+	"hot-coffee/internal/service/usecases"
+	"hot-coffee/internal/storage/postgres"
+	"hot-coffee/pkg/logger"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	l := logger.GetLogger()
+	
+	repo := postgres.NewPostgres()
+	serv := usecases.NewApplication(repo)
+	s := server.NewServer(serv)
+
+	if err := s.Serve(); err != nil {
+		l.Fatalf("error serving: %v", err.Error())
+	}
 }
